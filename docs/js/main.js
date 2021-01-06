2585bcbaba88f449962204595a9faf11f0c7ff10
Artem <lidama65@mail.ua>
2021-01-06 23:15:20 +0300

    unit

---
+++ docs/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        nav: true,
+        loop: true,
+        navText: ['',''],
+        navSpeed:1000
+            
+           
+    });
+  });
